d88b7a50411bf15b2c6455ac855b1203023a7890
Ibrahim Calzadilla <112896867+ibrahimhc19@users.noreply.github.com>
2025-06-06 09:11:08 -0500

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://acuafit.netlify.app'
     ],
 
     'allowed_origins_patterns' => [],
