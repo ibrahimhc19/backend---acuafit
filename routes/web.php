77e60a427c7b7f6443dd985aa90070cd9845848f
Ibrahim Calzadilla <112896867+ibrahimhc19@users.noreply.github.com>
2025-06-11 10:17:47 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,6 @@ Route::get('/exito', function () {
 // Estudiantes
 Route::post('/estudiante', [EstudianteController::class, 'store'])->middleware('auth:sanctum');
 Route::get('/api/estudiantes', [EstudianteController::class, 'index'])->middleware('auth:sanctum');
-Route::get('/public/estudiantes', [EstudianteController::class, 'index']);
 Route::get('/estudiante/{id}', [EstudianteController::class, 'show'])->middleware('auth:sanctum');
 Route::patch('/estudiante/{id}', [EstudianteController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('/estudiante/{id}', [EstudianteController::class, 'destroy'])->middleware('auth:sanctum');
