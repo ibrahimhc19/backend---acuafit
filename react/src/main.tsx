a1b1f3ac3ff024540e0aecfeb5a24dca32e05638
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-06-18 12:02:30 -0500

    Update build react

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import  AuthProvider  from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 
