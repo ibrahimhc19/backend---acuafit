729bb6ad29717c7c7b60dc82913c3f57f9016894
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-06-19 10:45:52 -0500

    Update build react, added modal prototype, changed minor css details, added new types and changed pagination links and page numbers, started creating the roles and permissions

--- react/src/api/axios.ts
+++ react/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://acuafit.up.railway.app/",
+    // baseURL: "https://acuafit.up.railway.app/",
+    baseURL: import.meta.env.VITE_APP_API_URL,
     withXSRFToken: true,
     withCredentials: true,
 });
