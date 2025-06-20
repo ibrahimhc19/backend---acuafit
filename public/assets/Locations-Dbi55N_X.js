2c46682239f83b6b608a76325ef3229c62f7ccd9
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-06-19 21:04:13 -0500

    Update build react

--- public/assets/Locations-Dbi55N_X.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CIUZC-rj.js";function t(){return e.jsx("div",{className:"container mx-auto p-2 sm:p-4 min-h-[75vh]",children:"Sedes"})}export{t as default};
---
+++ public/assets/Parents-MXT55pei.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-D51hdakU.js";const e=()=>s.jsx("div",{className:"container mx-auto p-2 sm:p-4 min-h-[75vh]",children:"Acudientes"});export{e as default};
