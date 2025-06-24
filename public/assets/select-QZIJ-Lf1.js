d6a1bcdc793d35c2758faff2ff06b7887274d69e
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-06-24 12:49:18 -0500

    Update react build and routes prefixes

--- public/assets/select-QZIJ-Lf1.js
+++ public/assets/select-QZIJ-Lf1.js
@@ -1,4 +1,4 @@
-import{h as st,R as he,g as Y,j as v,l as Ze,r as p,n as ct,P as z,o as J,m as It,c as Se}from"./index-BZuK4TwT.js";import{u as yt,a as lt,c as W,b as On,h as In,d as Nn,R as _n,F as Mn,D as Dn}from"./index-oWzKczDI.js";import{c as at}from"./createLucideIcon-CVH0bg0U.js";/**
+import{h as st,R as he,g as Y,j as v,l as Ze,r as p,n as ct,P as z,o as J,m as It,c as Se}from"./index-DJOu5f8a.js";import{u as yt,a as lt,c as W,b as On,h as In,d as Nn,R as _n,F as Mn,D as Dn}from"./index-BSPdWjc9.js";import{c as at}from"./createLucideIcon-UMzNJKOk.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
