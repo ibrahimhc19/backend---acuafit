b4b855cc31c44a34c9226257162dfce4a2aae77b
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-06-25 16:51:33 -0500

    Cambio en el modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -19,21 +20,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        "nombres",
-        "apellidos",
-        'tipo_documento',
-        'documento_identidad',
-        'correo',
-        'telefono',
-        'rut',
-        'avatar_url',
-        "avatar_initials",
-        "roles",
-        'google_id',
-    ];
+    'name',
+    'email',
+    'password',  
+    'google_id',
+    'avatar_url',
+    'avatar_initials',
+];
 
     /**
      * The attributes that should be hidden for serialization.
