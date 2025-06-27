fed73999031b6481ae396606247887199fac4269
Ibrahim Calzadilla <ibrahim.calzadilla@masglobalconsulting.com>
2025-06-27 13:23:04 -0500

    Update build react

--- public/assets/timeFormatter-DNfpeksr.js
+++
@@ -1,1 +0,0 @@
-const i=e=>{const[s,n,r]=e.split(":"),t=new Date;return t.setHours(parseInt(s),parseInt(n),parseInt(r)),t.toLocaleTimeString("en-US",{hour:"2-digit",minute:"2-digit",hour12:!0})};export{i as t};
