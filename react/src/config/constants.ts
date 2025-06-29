5a9eb44a3679111fc90da558b06ea0871c0a2038
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-06-28 23:19:02 -0500

    implementando estados en el formulario

--- react/src/config/constants.ts
+++ react/src/config/constants.ts
@@ -1,33 +1,33 @@
 export const DIAS_SEMANA = [
-  "Lunes",
-  "Martes",
-  "Miércoles",
-  "Jueves",
-  "Viernes",
-  "Sábado",
-  "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+    "Domingo",
 ];
 // Si se va a modificar también se debe cambiar el enum en el backend
 export const TIPOS_GRUPO = [
-  "Adultos",
-  "Hidroaeróbicos",
-  "Apnea",
-  "Niños",
-  "Niños 2-4 años",
-  "Niños 5-10 años",
-  "Bebés 10-23 meses",
+    "Adultos",
+    "Hidroaeróbicos",
+    "Apnea",
+    "Niños",
+    "Niños 2-4 años",
+    "Niños 5-10 años",
+    "Bebés 10-23 meses",
 ];
 
 export const TIPOS_DOCUMENTO = [
-  "Tarjeta de identidad",
-        "Registro civil",
-        "Cédula de ciudadanía",
-        "Documento extranjero",
+    "Tarjeta de identidad",
+    "Registro civil",
+    "Cédula de ciudadanía",
+    "Documento extranjero",
 ];
 
 export const FILAS = [
-        { label: "10", value: 10 },
-        { label: "15", value: 15 },
-        { label: "20", value: 20 },
-        { label: "30", value: 30 },
-    ];
+    { label: "10", value: 10 },
+    { label: "15", value: 15 },
+    { label: "20", value: 20 },
+    { label: "30", value: 30 },
+];
